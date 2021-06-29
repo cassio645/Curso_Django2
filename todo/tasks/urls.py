b29f3619976a323d99d614ee1b1cf9f5514d2316
cassio645 <cantonio645@gmail.com>
2021-06-29 17:22:57 -0300

    04 Urls, views, templates

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('helloworld', views.helloworld, name='helloworld'),
+    path('helloworld/', views.helloworld, name='helloworld'),
+    path('', views.taskList, name='task-list'),
+    path('yourname/<str:name>', views.yourname, name='your-name'),
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def helloworld(request):
     return HttpResponse("Hello, world.")
+
+
+def taskList(request):
+    return render(request, 'tasks/list.html')
+
+
+def yourname(request, name):
+    return render(request, 'tasks/yourname.html', {'name': name})
