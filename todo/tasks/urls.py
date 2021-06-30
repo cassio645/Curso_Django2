d0622f7911ffd962053af7cf271e7118a982e8bf
cassio645 <cantonio645@gmail.com>
2021-06-30 15:33:48 -0300

    10 Inserindo dados no banco de dados e criando nova tarefa p template

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('helloworld/', views.helloworld, name='helloworld'),
     path('', views.taskList, name='task-list'),
     path('task/<int:id>/', views.taskView, name='task-view'),
+    path('newtask/', views.newTask, name='new-task'),
     path('yourname/<str:name>', views.yourname, name='your-name'),
 ]
