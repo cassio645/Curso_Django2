efc73bb948eee91a432fb8dde4df7dc7e2cf45bb
cassio645 <cantonio645@gmail.com>
2021-07-01 09:26:01 -0300

    Editando dados no banco de dados

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect, render, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 
+
 def taskList(request):
     tasks = Task.objects.all().order_by('-created_at')
     return render(request, 'tasks/list.html', {'tasks': tasks})
@@ -12,6 +13,7 @@ def taskList(request):
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
+
 
 def newTask(request):
     if request.method == 'POST':
@@ -25,8 +27,25 @@ def newTask(request):
         form = TaskForm()
         return render(request, 'tasks/addtask.html', {'form': form})
 
+
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+
+    if(request.method == 'POST'):
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task.save()
+            return redirect('/')
+        else:
+            return render(request, 'tasks/editTask.html',{'form': form, 'task': task})
+    else:
+        return render(request, 'tasks/editTask.html',{'form': form, 'task': task})
+
+
 def helloworld(request):
     return HttpResponse("Hello, world.")
+
 
 def yourname(request, name):
     return render(request, 'tasks/yourname.html', {'name': name})
