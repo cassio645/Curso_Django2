6d569c9093529531928150b05e7eb61ee63cea11
cassio645 <cantonio645@gmail.com>
2021-07-02 09:41:42 -0300

    16 Registro de usuários

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('tasks.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
